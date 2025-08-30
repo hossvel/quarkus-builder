7487007f5a87fc60fe7bfaf5d13a14b356fef3f9
devhoss <velasco2288@gmail.com>
2025-08-30 11:43:23 -0500

    Factory method

--- src/main/java/com/hossvel/service/FacturaServiceImpl.java
+++ src/main/java/com/hossvel/service/FacturaServiceImpl.java
@@ -4,21 +4,21 @@ import com.hossvel.builder.FacturaDirector;
 import com.hossvel.builder.FacturaInternacionalBuilder;
 import com.hossvel.builder.FacturaNacionalBuilder;
 import com.hossvel.builder.IFacturaBuilder;
+import com.hossvel.factory.FacturaFactory;
 import com.hossvel.model.Factura;
 import com.hossvel.model.FacturaDTO;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class FacturaServiceImpl implements IFacturaService{
 
+    @Inject
+    FacturaFactory facturaFactory;
+
     public Factura construirFactura(FacturaDTO dto) {
 
-        IFacturaBuilder builder = switch (dto.tipo.toLowerCase()) {
-            case "nacional" -> new FacturaNacionalBuilder();
-            case "internacional" -> new FacturaInternacionalBuilder();
-            default -> throw new IllegalArgumentException("Tipo de factura no válido");
-        };
-
+        IFacturaBuilder builder = facturaFactory.createFactory(dto.tipo);
         FacturaDirector director = new FacturaDirector(builder);
         director.construirFactura(dto.cliente, dto.direccion, dto.subtotal);
         return director.getFactura();
